a78b103862056c23c6f1b3e5445c871922b09a26
Anuraag Agrawal <anuraaga@gmail.com>
2024-11-29 13:36:06 +0900

    Update homebrew formula

--- Formula/ollama-elastic.rb
+++ Formula/ollama-elastic.rb
@@ -4,8 +4,8 @@ class OllamaElastic < Formula
   version "dbeaea4"
 
   on_macos do
-    url "https://github.com/elastic/ollama/releases/download/dbeaea4/ollama-darwin"
-    sha256 "df241ff0b7f0f4ed3be36e2f9322848bd6b2a4dec205a84e0b104272a6393ffe"
+    url "https://github.com/elastic/ollama/releases/download/fcd811b/ollama-darwin"
+    sha256 "c399ca92c0225f32f0482ff4c111013136090b17d4b770bfc7a4c913d4a79cdc"
     def install
       bin.install "ollama-darwin" => "ollama-elastic"
     end
@@ -14,8 +14,8 @@ class OllamaElastic < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/elastic/ollama/releases/download/dbeaea4/ollama-linux-amd64.tgz"
-        sha256 "fa8429dccf0f484aab143e934cc3fa2ffb2d07519c62b380d3554d12ed5af6ec"
+        url "https://github.com/elastic/ollama/releases/download/fcd811b/ollama-linux-amd64.tgz"
+        sha256 "29e42fefa2328537c4fbcc5f0a0b52e872a803527364d07c61b1b2e21456cec9"
 
         def install
           bin.install "bin/ollama" => "ollama-elastic"
@@ -24,8 +24,8 @@ class OllamaElastic < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/elastic/ollama/releases/download/dbeaea4/ollama-linux-arm64.tgz"
-        sha256 "fc49eeeba27db672b1d56d91ccbe4f6ef853bc49162275d98f173e78ef1b1c48"
+        url "https://github.com/elastic/ollama/releases/download/fcd811b/ollama-linux-arm64.tgz"
+        sha256 "31c49bd76da9776b0fc7399a02f7451b95258c9f2dd9d6928776ccec887d5aa0"
 
         def install
           bin.install "bin/ollama" => "ollama-elastic"
